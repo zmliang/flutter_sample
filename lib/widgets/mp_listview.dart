6c828dc5efe9817e253fa1844187652561fb9f15
zhangmingliang@znds.com <zhangmingliang@znds.com>
2019-04-22 11:21:17 +0800

    optimze：给ListView 的item添加分割线

--- lib/widgets/mp_listview.dart
+++ lib/widgets/mp_listview.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flute_music_player/flute_music_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/data/song_data.dart';
 import 'package:flutter_app/pages/now_playing.dart';
@@ -12,6 +13,7 @@ class MPListView extends StatelessWidget {
   Widget build(BuildContext context) {
     final rootIW = MPInheritedWidget.of(context);
     SongData songData = rootIW.songData;
+
     return new ListView.builder(
         itemCount: songData.length,
         itemBuilder: (context, int index) {
@@ -21,23 +23,34 @@ class MPListView extends StatelessWidget {
               ? null
               : new File.fromUri(Uri.parse(s.albumArt));
 
-          return new ListTile(
-            dense: false,
-            leading:
-                new Hero(child: avatar(artFile, s.title, color), tag: s.title),
-            title: new Text(s.title),
-            subtitle: Text(
-              "By ${s.artist}",
-              style: Theme.of(context).textTheme.caption,
+          return new Container(
+            child: new Column(
+              children: <Widget>[
+                buildItemData(color, artFile, s, context, songData, index),
+                new Divider(),
+              ],
             ),
-            onTap: () {
-              songData.setCurrentIndex(index);
-              Navigator.push(
-                  context,
-                  new MaterialPageRoute(
-                      builder: (context) => new NowPlaying(songData, s)));
-            },
           );
         });
+  }
+
+  Widget buildItemData(MaterialColor color, File artFile, Song s,
+      BuildContext context, SongData songData, int index) {
+    return new ListTile(
+      dense: false,
+      leading: new Hero(child: avatar(artFile, s.title, color), tag: s.title),
+      title: new Text(s.title),
+      subtitle: Text(
+        "By ${s.artist}",
+        style: Theme.of(context).textTheme.caption,
+      ),
+      onTap: () {
+        songData.setCurrentIndex(index);
+        Navigator.push(
+            context,
+            new MaterialPageRoute(
+                builder: (context) => new NowPlaying(songData, s)));
+      },
+    );
   }
 }
